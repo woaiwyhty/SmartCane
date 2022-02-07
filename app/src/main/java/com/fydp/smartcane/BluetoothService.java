d7393d1049a07bef6ac935cd279b2652a37df12d
Shihui Wang <s672wang@uwaterloo.ca>
2022-02-07 16:21:11 -0500

    bluetooth connection logic done. need data transfer now

---
+++ app/src/main/java/com/fydp/smartcane/BluetoothService.java
@@ -0,0 +1,190 @@
+package com.fydp.smartcane;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+
+public class BluetoothService {
+    // TODO: add voice output for all changes to bt_status
+
+    private TextView bt_status;
+    private Button bt_conn_button;
+    private Context context;
+
+    private BluetoothAdapter bt_adapter;
+    private BluetoothSocket bt_socket;
+    private BluetoothDevice bt_pi;
+
+    private final int BT_CONN_REQ_CODE = 1;
+    private final String TAG = "BluetoothService";
+
+    public BluetoothService(TextView bt_status, Button bt_conn_button, Context context) {
+        this.bt_status = bt_status;
+        this.bt_conn_button = bt_conn_button;
+        this.context = context;
+        this.bt_adapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public boolean hasBtPermission() {
+        return (ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_ADMIN) == PackageManager.PERMISSION_GRANTED);
+    }
+    public void getBtPermissions() {
+        if (!hasBtPermission()) {
+            this.bt_status.setText("requesting bt discoverable permission");
+            Log.d(TAG, "requesting bt discoverable permission");
+            String[] bt_permissions = {
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.BLUETOOTH,
+            };
+            ActivityCompat.requestPermissions((Activity) context, bt_permissions, BT_CONN_REQ_CODE);
+        } else {
+            this.bt_status.setText("bt permission already granted");
+            Log.d(TAG, "bt permission already granted");
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private boolean findPi(String pi_name) {
+        if (!hasBtPermission()) {
+            this.bt_status.setText("bt permission not granted, please grant permission and try again");
+            Log.d(TAG, "bt permission not granted, please grant permission and try again");
+            getBtPermissions();
+            return false;
+        }
+        Set<BluetoothDevice> pairedDevices = bt_adapter.getBondedDevices();
+
+        // There are paired devices. Get the name and address of each paired device.
+        for (BluetoothDevice device : pairedDevices) {
+            this.bt_status.setText("Looking for the cane");
+            Log.d(TAG, "Looking for the cane");
+            String deviceName = device.getName();
+//            String deviceHardwareAddress = device.getAddress(); // MAC address
+            if (deviceName.equals(pi_name)) {
+                this.bt_status.setText("Found the cane in paired devices");
+                Log.d(TAG, "Found the cane in paired devices");
+                this.bt_pi = device;
+                return true;
+            }
+        }
+
+        this.bt_status.setText("Cannot find your cane. Please pair it with your phone and try again");
+        Log.d(TAG, "Cannot find your cane. Please pair it with your phone and try again");
+        return false;
+    }
+
+    @SuppressLint("MissingPermission")
+    public void connectToPi(String pi_name) {
+        bt_conn_button.setEnabled(false);
+        if (!findPi(pi_name)) {
+            return;
+        }
+
+        // Cancel discovery because it otherwise slows down the connection.
+        bt_adapter.cancelDiscovery();
+
+        // Connect to the remote device through the socket. connect() blocks
+        // until it succeeds or throws an exception. So use a countdowntimer
+        // to try repeatedly
+        // try to connect for 10 times, each with 5 sec interval
+        bt_status.setText("Connecting to cane. Make sure it's on and in range");
+        (new ConnectThread()).start();
+        new CountDownTimer(50000, 5000) {
+            private void onConnected() {
+                bt_status.setText("Established bluetooth connection to the cane");
+                Log.d(TAG, "Established bluetooth connection to the cane");
+                bt_conn_button.setText("Already connected to cane");
+                bt_conn_button.setEnabled(false);
+                // call func to start receiver thread
+            }
+
+            public void onTick(long millisUntilFinished) {
+                if (bt_socket.isConnected()) {
+                    onConnected();
+                    cancel(); // cancel the timer
+                    return;
+                }
+
+                // try again
+                try {
+                    bt_socket.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "Could not close the client socket", e);
+                }
+                (new ConnectThread()).start();
+            }
+
+            public void onFinish() {
+                if (bt_socket.isConnected()) {
+                    onConnected();
+                    return;
+                }
+                try {
+                    bt_socket.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "Could not close the client socket", e);
+                }
+                bt_conn_button.setEnabled(true);
+                bt_status.setText("Cannot connect to the cane. Please try again");
+                Log.d(TAG, "Cannot connect to the cane. Please try again");
+            }
+        }.start();
+    }
+
+
+    private class ConnectThread extends Thread {
+        @SuppressLint("MissingPermission")
+        public ConnectThread() {
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                // UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID?
+                // UUID uuid = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee"); //Standard SerialPortService ID?
+                UUID uuid = UUID.fromString("815425a5-bfac-47bf-9321-c5ff980b5e11");
+                bt_socket = bt_pi.createRfcommSocketToServiceRecord(uuid);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+        }
+
+        @SuppressLint("MissingPermission")
+        public void run() {
+            try {
+                bt_socket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    bt_socket.close();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                bt_socket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+    }
+}
--- app/src/main/java/com/fydp/smartcane/MainActivity.java
+++ app/src/main/java/com/fydp/smartcane/MainActivity.java
@@ -3,6 +3,7 @@ package com.fydp.smartcane;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
@@ -31,6 +32,7 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Set;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -52,8 +54,14 @@ public class MainActivity extends AppCompatActivity {
 
         initContentMain();
         initLocationPermission();
-
-        initBluetoothPermission();
+        this.bt_service = new BluetoothService(this.bluetooth_conn_status, this.bt_conn_button, MainActivity.this);
+        this.bt_service.getBtPermissions();
+        this.bt_conn_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bt_service.connectToPi(PI_NAME);
+            }
+        });
     }
 
     @Override
@@ -76,47 +84,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        try {
-            super.onActivityResult(requestCode, resultCode, data);
-
-            if (resultCode == RESULT_CANCELED) {
-                Log.d(Integer.toString(requestCode), "request denied");
-                return;
-            }
-
-            switch (requestCode) {
-                case BT_CONN_REQ_CODE:
-                    this.bluetooth_conn_status.setText("bt discoverable permissions granted");
-                    wait_for_pi_connection();
-            }
-        } catch (Exception ex) {
-            Log.d(Integer.toString(requestCode), "exception in processing request");
-        }
-    }
-
-
-    @SuppressWarnings("deprecation")
-    private void initBluetoothPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
-            this.bluetooth_conn_status.setText("requesting bt discoverable permission");
-            String[] bt_permissions = {
-                Manifest.permission.BLUETOOTH_ADMIN,
-                Manifest.permission.BLUETOOTH,
-            };
-            ActivityCompat.requestPermissions(this, bt_permissions, BT_CONN_REQ_CODE);
-//            Intent discoverableIntent =
-//                    new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-//            // wait for 5 minutes
-//            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-//            startActivityForResult(discoverableIntent, BT_CONN_REQ_CODE);
-        } else {
-            this.bluetooth_conn_status.setText("bt permission already granted");
-        }
-        wait_for_pi_connection();
     }
 
     private void initLocationPermission() {
@@ -164,76 +131,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
         this.bluetooth_conn_status = findViewById(R.id.bluetooth_conn_status);
+        this.bt_conn_button = findViewById(R.id.bt_conn_button);
     }
 
-    @SuppressLint("MissingPermission")
-    private void wait_for_pi_connection() {
-        bluetooth_conn_status.setText("Waiting for smart cane to come online");
-        // TODO: voice output the above as well
-        this.bt_adapter = BluetoothAdapter.getDefaultAdapter();
-        class AcceptThread extends Thread {
-            private BluetoothServerSocket bt_server_socket = null;
+//    @SuppressLint("MissingPermission")
+//    @SuppressWarnings("deprecation")
+//    private void wait_for_pi_connection() {
+//        Intent discoverableIntent =
+//                    new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+//        // wait for 5 minutes
+//        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+//        startActivityForResult(discoverableIntent, BT_CONN_REQ_CODE);
 
-            public AcceptThread() {
-                try {
-                    // MY_UUID is the app's UUID string, also used by the client code.
-                    //UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID?
-                    UUID uuid = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee"); //Standard SerialPortService ID?
-                    this.bt_server_socket = bt_adapter.listenUsingRfcommWithServiceRecord("smartcane-phone", uuid);
-                } catch (IOException e) {
-                    Log.e("socket", "Socket's listen() method failed", e);
-                }
+        // look for pi, which should be paired already
 
-            }
 
-            public void run() {
-                BluetoothSocket socket = null;
-                // Keep listening until exception occurs or a socket is returned.
-                while (true) {
-                    try {
-                        bluetooth_conn_status.setText("listening on socket");
-                        socket = bt_server_socket.accept();
-                    } catch (IOException e) {
-                        bluetooth_conn_status.setText("exception accepting connection");
-                        break;
-                    }
+//                        InputStream tmpIn = bt_socket.getInputStream();
+//                        byte[] buf;
+//                        buf = new byte[1024];
+//                        int numBytes; // bytes returned from read()
+//                        // Read from the InputStream.
+//                        numBytes = tmpIn.read(buf);
+//                        String str = new String(buf, StandardCharsets.UTF_8);
+//                        this.bluetooth_conn_status.setText(str);
 
-                    if (socket != null) {
-                        // A connection was accepted. Perform work associated with
-                        // the connection in a separate thread.
-                        bluetooth_conn_status.setText("connection accepted!");
-                        try {
-                            bt_server_socket.close();
-                            InputStream tmpIn = socket.getInputStream();
-                            byte[] buf;
-                            while (true) {
-                                buf = new byte[1024];
-                                int numBytes; // bytes returned from read()
-                                // Read from the InputStream.
-                                numBytes = tmpIn.read(buf);
-                                String str = new String(buf, StandardCharsets.UTF_8);
-                                bluetooth_conn_status.setText(str);
-                            }
-                        } catch (IOException e) {
-                            bluetooth_conn_status.setText("exception reading from connection");
-                            break;
-                        }
-                    }
-                }
-            }
 
-            // Closes the connect socket and causes the thread to finish.
-            public void cancel() {
-                try {
-                    bt_server_socket.close();
-                } catch (IOException e) {
-                    Log.e("socket", "Could not close the connect socket", e);
-                }
-            }
-        }
-
-        (new Thread(new AcceptThread())).start();
-    }
+//    }
 
     private void setNotification(String message) {
         tv_notification.setText(message);
@@ -249,7 +172,8 @@ public class MainActivity extends AppCompatActivity {
     private Button button_test_gps;
 
     // connect to bluetooth
-    private static final int BT_CONN_REQ_CODE = 1;
     private TextView bluetooth_conn_status;
-    private BluetoothAdapter bt_adapter;
+    private Button bt_conn_button;
+    private BluetoothService bt_service;
+    private final String PI_NAME = "raspberrypi-61";
 }
