513a0c7ab69d7a8e25a9a66d419b919d066a6ca2
Wenqian Chen <qianxiongzaizai@126.com>
2022-02-28 12:43:11 -0500

    missing lidar protocol, everything else works

--- app/src/main/java/com/fydp/smartcane/BluetoothService.java
+++ app/src/main/java/com/fydp/smartcane/BluetoothService.java
@@ -33,6 +33,13 @@ public class BluetoothService {
     private BluetoothSocket bt_socket;
     private BluetoothDevice bt_pi;
 
+    private TTS tts;
+
+    private VoiceInputService voiceInputService;
+
+    private boolean emg_on;
+    private boolean emg_current_state;
+
     private final int BT_CONN_REQ_CODE = 1;
     private final String TAG = "BluetoothService";
 
@@ -41,6 +48,10 @@ public class BluetoothService {
         this.bt_conn_button = bt_conn_button;
         this.context = context;
         this.bt_adapter = BluetoothAdapter.getDefaultAdapter();
+        this.tts = TTS.getTTS();
+        this.emg_on = false;
+        this.emg_current_state = false;
+        this.voiceInputService = VoiceInputService.getInstance(this.bt_status, context);
     }
 
     public boolean hasBtPermission() {
@@ -50,6 +61,7 @@ public class BluetoothService {
     public void getBtPermissions() {
         if (!hasBtPermission()) {
             this.bt_status.setText("requesting bt permissions");
+            this.tts.textToVoice("requesting bt permissions");
             Log.d(TAG, "requesting bt permissions");
             String[] bt_permissions = {
                     Manifest.permission.BLUETOOTH_ADMIN,
@@ -58,6 +70,7 @@ public class BluetoothService {
             ActivityCompat.requestPermissions((Activity) context, bt_permissions, BT_CONN_REQ_CODE);
         } else {
             this.bt_status.setText("bt permission already granted");
+            this.tts.textToVoice("bt permission already granted");
             Log.d(TAG, "bt permission already granted");
         }
     }
@@ -66,6 +79,7 @@ public class BluetoothService {
     private boolean findPi(String pi_name) {
         if (!hasBtPermission()) {
             this.bt_status.setText("bt permission not granted, please grant permission and try again");
+            this.tts.textToVoice("bt permission not granted, please grant permission and try again");
             Log.d(TAG, "bt permission not granted, please grant permission and try again");
             getBtPermissions();
             return false;
@@ -75,11 +89,13 @@ public class BluetoothService {
         // There are paired devices. Get the name and address of each paired device.
         for (BluetoothDevice device : pairedDevices) {
             this.bt_status.setText("Looking for the cane");
+            this.tts.textToVoice("Looking for the cane");
             Log.d(TAG, "Looking for the cane");
             String deviceName = device.getName();
 //            String deviceHardwareAddress = device.getAddress(); // MAC address
             if (deviceName.equals(pi_name)) {
                 this.bt_status.setText("Found the cane in paired devices");
+                this.tts.textToVoice("Found the cane in paired devices");
                 Log.d(TAG, "Found the cane in paired devices");
                 this.bt_pi = device;
                 return true;
@@ -87,6 +103,7 @@ public class BluetoothService {
         }
 
         this.bt_status.setText("Cannot find your cane. Please pair it with your phone and try again");
+        this.tts.textToVoice("Cannot find your cane. Please pair it with your phone and try again");
         Log.d(TAG, "Cannot find your cane. Please pair it with your phone and try again");
         return false;
     }
@@ -112,10 +129,12 @@ public class BluetoothService {
         // to try repeatedly
         // try to connect for 10 times, each with 5 sec interval
         bt_status.setText("Connecting to cane. Make sure it's on and in range");
+        tts.textToVoice("Connecting to cane. Make sure it's on and in range");
         (new ConnectThread()).start();
         new CountDownTimer(50000, 5000) {
             private void onConnected() {
                 bt_status.setText("Established bluetooth connection to the cane");
+                tts.textToVoice("Established bluetooth connection to the cane");
                 Log.d(TAG, "Established bluetooth connection to the cane");
                 bt_conn_button.setText("Already connected to cane");
                 bt_conn_button.setEnabled(false);
@@ -150,30 +169,201 @@ public class BluetoothService {
                 }
                 bt_conn_button.setEnabled(true);
                 bt_status.setText("Cannot connect to the cane. Please try again");
+                tts.textToVoice("Cannot connect to the cane. Please try again");
                 Log.d(TAG, "Cannot connect to the cane. Please try again");
             }
         }.start();
     }
 
-    private void processMsg(String msg) {
-        // TODO: change this to more meaningful processing
-        // Could potentially put this is a separate thread if neccessary?
+    private class emergencyProtocolThread extends Thread {
 
-        // 1. emergency button pressed
-        //    - if is_pressed == false -> init emergency protocol (play sound etc)
-        //    - if is_pressed == true -> turn off emergency protocol
-
-        // 2. lidar input received
-
-        // updates to the ui must be done on the main thread
-        ((Activity) context).runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-
-                bt_status.setText(msg);
-
+        public void run(){
+            try{
+                if (emg_on == false){
+                    return;
+                }
+                ((Activity) context).runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        bt_status.setText("An emergency happened. Need help. Please call 911.");
+                        Log.d(TAG, "An emergency happened. Need help. Please call 911.");
+                    }
+                });
+                while(true){
+                    if (emg_on == false){
+                        return;
+                    }
+                    ((Activity) context).runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tts.textToVoice("An emergency happened. Need help. Please call 911.");
+                        }
+                    });
+                    sleep(9000);
+                }
+            }catch(Exception e){
+                Log.d(TAG, "Failed to launch emergency protocol.", e);
             }
-        });
+        }
+    }
+
+    private class msgProcessThread extends Thread {
+        private String[] parsedMsg;
+
+        public msgProcessThread(String[] parsedMsg){
+            this.parsedMsg = parsedMsg;
+        }
+
+        private void processMsg(String[] parsedMsg) throws Exception {
+            // parsedMsg = "NAME:data"
+
+            // 1. emergency button pressed
+            //    - if is_pressed == false -> init emergency protocol (play sound etc) call TTS
+            //    - if is_pressed == true -> turn off emergency protocol
+            // 2. lidar input received
+            // 3. Press to speak
+
+            String name = parsedMsg[0];
+            switch(name) {
+                case "emergencyButton":
+                    try{
+                        if(parsedMsg[1].equals("true")){
+                            emg_on = true;
+                            if(emg_on == emg_current_state){
+                                Log.d(TAG, parsedMsg[0] + ":" + parsedMsg[1] + " sent twice. Emergency protocol remain unchanged.");
+                                throw new Exception(parsedMsg[0] + ":" + parsedMsg[1] + "Emergency protocol remain unchanged.");
+                            }
+
+                            // update and launch emergency protocol
+                            emg_current_state = emg_on;
+                            (new emergencyProtocolThread()).start();
+                        }
+
+                        if(parsedMsg[1].equals("false")){
+                            //set emergency protocol tp off and notify
+                            emg_on = false;
+                            if(emg_on == emg_current_state){
+                                Log.d(TAG, parsedMsg[0] + ":" + parsedMsg[1] + " sent twice. Emergency protocol remain unchanged.");
+                                throw new Exception(parsedMsg[0] + ":" + parsedMsg[1] + "Emergency protocol remain unchanged.");
+                            }
+
+                            emg_current_state = emg_on;
+                            ((Activity) context).runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    bt_status.setText("Emergency situation has been resolved. Thank you.");
+                                    tts.textToVoice("Emergency situation has been resolved. Thank you.");
+                                    Log.d(TAG, "Emergency situation has been resolved. Thank you.");
+                                }
+                            });
+                        }
+
+                        if(!parsedMsg[1].equals("false") || !parsedMsg[1].equals("true")){
+                            Log.d(TAG, "Invalid emergency button status.");
+                            throw new Exception("Invalid emergency button status.");
+                        }
+                    }catch(InterruptedException e){
+                        Log.d(TAG,"previous emergency protocol interrupted.", e);
+                    }
+                    break;
+                case "lidar":
+                    float distance_unit_cm = Float.parseFloat(parsedMsg[1]);
+                    // might be useful
+                    float lidar_strength = Float.parseFloat(parsedMsg[2]);
+                    float lidar_temperature = Float.parseFloat(parsedMsg[3]);
+
+
+                    // TODO: need to be connected to distance alarming/OpenCV service
+                    // Note: lidar sending speed REALLY FAST! careful about queue etc
+                    // This is a Dummy block for testing. Need to be changed to meaningful calls
+//                    if (distance_unit_cm < 20){
+////                        ((Activity) context).runOnUiThread(new Runnable() {
+////                            @Override
+////                            public void run() {
+////                                bt_status.setText(
+////                                        "Lidar: distance = " + distance_unit_cm + " cm" +
+////                                                ", strength = " + lidar_strength +
+////                                                ", temperature = " + lidar_temperature);
+////                            }
+////                        });
+//                        Log.d(TAG, "Lidar: distance = " + distance_unit_cm + " cm" +
+//                                        ", strength = " + lidar_strength +
+//                                        ", temperature = " + lidar_temperature);
+//                        Log.d(TAG, "original msg: " + parsedMsg[0] + " : " + parsedMsg[1] + " : " + parsedMsg[2] + " : " + parsedMsg[3]);
+//                    }
+                    Log.d(TAG, "Lidar: distance = " + distance_unit_cm + " cm" +
+                            ", strength = " + lidar_strength +
+                            ", temperature = " + lidar_temperature);
+                    Log.d(TAG, "original msg: " + parsedMsg[0] + " : " + parsedMsg[1] + " : " + parsedMsg[2] + " : " + parsedMsg[3]);
+                    break;
+                case "pressToSpeak":
+                    boolean pressToSpeak;
+
+                    // TODO: press to speak working. Need to connect the input voice msg to follow up protocols
+                    if(parsedMsg[1].equals("true")){
+                        // This is a Dummy test of Voice Input start listening
+                        pressToSpeak = true;
+                        ((Activity) context).runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                voiceInputService.startListening();
+                                bt_status.setText("");
+                                bt_status.setHint("Listening...");
+                            }
+                        });
+                    }else if(parsedMsg[1].equals("false")){
+                        // This is a Dummy test of Voice Input stop listening
+                        pressToSpeak = false;
+                        ((Activity) context).runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                bt_status.setHint("You will see input here");
+                                voiceInputService.stopListening();
+                            }
+                        });
+                    }else{
+                        throw new Exception("Invalid press to speak status.");
+                    }
+                    break;
+                case "lowBattery":
+                    boolean bettery_low;
+                    if(parsedMsg[1].equals("true")){
+                        bettery_low = true;
+                        ((Activity) context).runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                bt_status.setText("Battery low. Please charge now.");
+                                tts.textToVoice("Battery low. Please charge now.");
+                                Log.d(TAG, "Battery low. Please charge now.");
+                            }
+                        });
+                    }else if(parsedMsg[1].equals("true")){
+                        bettery_low = false;
+                        ((Activity) context).runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                bt_status.setText("bettery_low = false");
+                                tts.textToVoice("bettery_low = false");
+                                Log.d(TAG, "bettery_low = false");
+                            }
+                        });
+                    }
+
+                    break;
+                default:
+                    // unrecognized type
+                    Log.d(TAG, "Unrecognized message type: " + parsedMsg[0]);
+                    throw new Exception("Unrecognized message type: " + parsedMsg[0]);
+            }
+        }
+
+        public void run(){
+            try{
+                this.processMsg(parsedMsg);
+            }catch(Exception e){
+                Log.d(TAG, "Message process failed.", e);
+            }
+        }
     }
 
     private class ConnectThread extends Thread {
@@ -238,8 +428,19 @@ public class BluetoothService {
                     // Read from the InputStream.
                     numBytes = inputStream.read(buf);
                     String msg = new String(buf, StandardCharsets.UTF_8);
-                    processMsg(msg);
+                    String[] cleanMsg = msg.split(";");
+                    String[] parsedMsg = cleanMsg[0].split(":");
+                    (new msgProcessThread(parsedMsg)).start();
                 } catch (IOException e) {
+                    ((Activity) context).runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            bt_status.setText("bluetooth connection lost");
+                            tts.textToVoice("bluetooth connection lost");
+                            bt_conn_button.setText("Connect to cane");
+                            bt_conn_button.setEnabled(true);
+                        }
+                    });
                     Log.d(TAG, "Input stream was disconnected", e);
                     break;
                 }
