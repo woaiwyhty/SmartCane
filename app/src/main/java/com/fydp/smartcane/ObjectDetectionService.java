412a89f84bfc8f8ef84bddca2a77da5dd4c6b0bb
shuyirt <shuyi.tao@uwaterloo.ca>
2022-03-04 19:39:10 -0500

    new change

--- app/src/main/java/com/fydp/smartcane/ObjectDetectionService.java
+++ app/src/main/java/com/fydp/smartcane/ObjectDetectionService.java
@@ -4,10 +4,9 @@ import android.util.Log;
 
 public class ObjectDetectionService {
     enum ObjectState {
-        IDLE(1000),              // d > 10m
-        LEVEL_SUPER_FAR(500),    // 5m < d <= 10m
-        LEVEL_FAR(300),    // 5m < d <= 10m
-        LEVEL_APPROACHING(100),  // 1m < d <= 5m
+        IDLE(1100),              // d > 11m
+        LEVEL_SUPER_FAR(600),    // 6m < d <= 11m
+        LEVEL_APPROACHING(200),  // 1m < d <= 5m 提前预判空间
         LEVEL_CLOSE(0);          // 0m < d <= 1m
 
         int distance;
@@ -44,11 +43,14 @@ public class ObjectDetectionService {
         }
         // compute the incoming state based on distance
         ObjectState incomingState = this.computeState(distance);
-        int THRESHOLD = 100;
+        int THRESHOLD = 50;
         upperBound = distance + THRESHOLD;
         lowerBound = distance - THRESHOLD;
         Log.d("ObjectDetectionService", "Lidar: distance = " + distance + " cm, state: " + incomingState );
-        playWarning(incomingState);
+        if (lastState != incomingState) {
+            playWarning(incomingState);
+            lastState = incomingState;
+        }
     }
 
     /*
@@ -60,8 +62,6 @@ public class ObjectDetectionService {
             nextState = ObjectState.IDLE;
         } else if (distance > ObjectState.LEVEL_SUPER_FAR.getDistance()) {
             nextState = ObjectState.LEVEL_SUPER_FAR;
-        }  else if (distance > ObjectState.LEVEL_FAR.getDistance()) {
-            nextState = ObjectState.LEVEL_FAR;
         } else if (distance > ObjectState.LEVEL_APPROACHING.getDistance()) {
             nextState = ObjectState.LEVEL_APPROACHING;
         } else {
@@ -70,31 +70,22 @@ public class ObjectDetectionService {
         return nextState;
     }
 
-    private void playWarning(ObjectState nextState) {
-        if (lastState != nextState) {
-            switch (nextState) {
-                case IDLE:
-                    TTS.getTTS().textToVoice("Object is 10 meters away");
-                    break;
-                case LEVEL_SUPER_FAR:
-                    TTS.getTTS().textToVoice("Object is 5 meters away");
-                    break;
-                case LEVEL_FAR:
-                    TTS.getTTS().textToVoice("Object is 3 meters away");
-                    break;
-                case LEVEL_APPROACHING:
-                    TTS.getTTS().textToVoice("Attention! An object is approaching");
-                    break;
-                case LEVEL_CLOSE:
-                    TTS.getTTS().textToVoice("Attention. Object is nearby.");
-                    TTS.getTTS().textToVoice("Beep Beep Beep Beep Beep");
-                    break;
-                default:
-                    TTS.getTTS().textToVoice("Attention!");
-            }
+    private void playWarning(ObjectState state) {
+        switch (state) {
+            case LEVEL_SUPER_FAR:
+                // 5 < d < 10
+                TTS.getTTS().textToVoice("10 meters alert");
+                break;
+            case LEVEL_APPROACHING:
+                // 2 < d < 5
+                TTS.getTTS().textToVoice("5 meters alert.");
+                break;
+            case LEVEL_CLOSE:
+                // d < 2
+                TTS.getTTS().textToVoice("Beep Beep Beep Beep Beep");
+                break;
+            default:
+                break;
         }
-        lastState = nextState;
     }
-
-
 }
